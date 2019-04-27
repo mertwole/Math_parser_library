c9af48c6a0c6d3c9cc5537b2fa9970af209ece2d
mertwole <mertwole@gmail.com>
2019-04-27 21:48:43 +0300

    initial commit

--- Form1.cs
+++ Form1.cs
@@ -15,5 +15,10 @@ namespace Math_parser_library
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
